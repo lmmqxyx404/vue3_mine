f93c8a705a31d051aefdafa9d0558b162a20410d
lmmqxyx <1368470804@qq.com>
2023-08-10 15:33:06 +0800

    add prettier support

--- scripts/dev.js
+++ scripts/dev.js
@@ -4,7 +4,10 @@
 // We are still using Rollup for production builds because it generates
 // smaller files w/ better tree-shaking.
 
-import { createRequire } from "node:module";
+import { createRequire } from 'node:module'
+import { resolve, relative, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+const require = createRequire(import.meta.url)
+console.log(require, import.meta.url)
 
-const require = createRequire(import.meta.url);
-console.log(require, import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url))
