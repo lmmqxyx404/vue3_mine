2df2e254f1b80c65e280db9879e4d2d00731f9c3
lmmqxyx <1368470804@qq.com>
2023-08-11 23:51:13 +0800

    prepare to improve the vue package

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -2,3 +2,7 @@
 
 // Global compile-time constants
 declare var __DEV__: boolean
+
+// used in vue/src/dev.ts
+declare var __BROWSER__: boolean
+declare var __ESM_BUNDLER__: boolean
---
+++ packages/vue/src/dev.ts
@@ -0,0 +1,12 @@
+export function initDev() {
+  if (__BROWSER__) {
+    if (!__ESM_BUNDLER__) {
+      console.info(
+        `You are running a development build of Vue.\n` +
+          `Make sure to use the production build (*.prod.js) when deploying for production.`
+      )
+    }
+
+    
+  }
+}
