d7afbf0d9e7c0d77806f74d56abfb5a189c914ef
lmmqxyx <1368470804@qq.com>
2023-08-12 23:40:13 +0800

    prepare to complete the build process.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,68 @@
 // @ts-check
 import { createRequire } from 'node:module'
+import { fileURLToPath } from 'node:url'
+import path, { dirname } from 'node:path'
+import { constEnum } from './scripts/const-enum'
 
 // 必须指定环境
 if (!process.env.TARGET) {
   throw new Error('TARGET package must be specified via --environment flag.')
+}
+
+const require = createRequire(import.meta.url)
+// 当前工程文件夹目录
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+const masterVersion = require('./package.json').version
+const consolidatePkg = require('@vue/consolidate/package.json')
+
+// 所有 packages 目录
+const packagesDir = path.resolve(__dirname, 'packages')
+// 获取 package 目录路径
+const packageDir = path.resolve(packagesDir, process.env.TARGET)
+
+// 定义一个函数 resolve
+const resolve = p => path.resolve(packageDir, p)
+// 将 json 文件转化为JS对象
+const pkg = require(resolve(`package.json`))
+// 得到 buildOptions , 也就是获得了打包配置
+const packageOptions = pkg.buildOptions || {}
+// 得到包名 两种方式.
+const name = packageOptions.filename || path.basename(packageDir)
+
+const [enumPlugin, enumDefines] = constEnum()
+
+
+// 配置输出格式
+const outputConfigs = {
+  'esm-bundler': {
+    file: resolve(`dist/${name}.esm-bundler.js`),
+    format: `es`
+  },
+  'esm-browser': {
+    file: resolve(`dist/${name}.esm-browser.js`),
+    format: `es`
+  },
+  cjs: {
+    file: resolve(`dist/${name}.cjs.js`),
+    format: `cjs`
+  },
+  global: {
+    file: resolve(`dist/${name}.global.js`),
+    // 立即执行
+    format: `iife`
+  },
+  // runtime-only builds, for main "vue" package only
+  'esm-bundler-runtime': {
+    file: resolve(`dist/${name}.runtime.esm-bundler.js`),
+    format: `es`
+  },
+  'esm-browser-runtime': {
+    file: resolve(`dist/${name}.runtime.esm-browser.js`),
+    format: 'es'
+  },
+  'global-runtime': {
+    file: resolve(`dist/${name}.runtime.global.js`),
+    format: 'iife'
+  }
 }
---
+++ scripts/build.js
@@ -0,0 +1,120 @@
+// @ts-check
+
+/*
+Produces production builds and stitches together d.ts files.
+
+To specify the package to build, simply pass its name and the desired build
+formats to output (defaults to `buildOptions.formats` specified in that package,
+or "esm,cjs"):
+
+```
+# name supports fuzzy match. will build all packages with name containing "dom":
+nr build dom
+
+# specify the format to output
+nr build core --formats cjs
+```
+*/
+
+import fs from 'node:fs/promises'
+import { createRequire } from 'node:module'
+import minimist from 'minimist'
+import execa from 'execa'
+// used in run()
+import { scanEnums } from './const-enum.js'
+import { targets as allTargets, fuzzyMatchTarget } from './utils.js'
+import { cpus } from 'node:os'
+import path from 'node:path'
+import { existsSync } from 'node:fs'
+
+// use node:module
+const require = createRequire(import.meta.url)
+// use minimist
+const args = minimist(process.argv.slice(2))
+
+const targets = args._
+const formats = args.formats || args.f
+const devOnly = args.devOnly || args.d
+const prodOnly = !devOnly && (args.prodOnly || args.p)
+const buildTypes = args.withTypes || args.t
+const sourceMap = args.sourcemap || args.s
+const isRelease = args.release
+const buildAllMatching = args.all || args.a
+
+// use execa
+const commit = execa.sync('git', ['rev-parse', 'HEAD']).stdout.slice(0, 7)
+
+run()
+
+async function run() {
+  // use current dir
+  const removeCache = scanEnums()
+
+  try {
+    const resolvedTargets = targets.length
+      ? fuzzyMatchTarget(targets, buildAllMatching)
+      : allTargets
+    await buildAll(resolvedTargets)
+  } finally {
+    removeCache()
+  }
+}
+
+async function buildAll(targets) {
+  await runParallel(cpus().length, targets, build)
+}
+
+async function runParallel(maxConcurrency, source, iteratorFn) {
+  const ret = []
+  const executing = []
+  for (const item of source) {
+    const p = Promise.resolve().then(() => iteratorFn(item, source))
+    ret.push(p)
+
+    if (maxConcurrency <= source.length) {
+      const e = p.then(() => executing.splice(executing.indexOf(e), 1))
+      executing.push(e)
+      if (executing.length >= maxConcurrency) {
+        await Promise.race(executing)
+      }
+    }
+  }
+  return Promise.all(ret)
+}
+
+async function build(target) {
+  const pkgDir = path.resolve(`packages/${target}`)
+  const pkg = require(`${pkgDir}/package.json`)
+
+  // if this is a full build (no specific targets), ignore private packages
+  if ((isRelease || !targets.length) && pkg.private) {
+    return
+  }
+
+  // if building a specific format, do not remove dist.
+  if (!formats && existsSync(`${pkgDir}/dist`)) {
+    await fs.rm(`${pkgDir}/dist`, { recursive: true })
+  }
+
+  const env =
+    (pkg.buildOptions && pkg.buildOptions.env) ||
+    (devOnly ? 'development' : 'production')
+  await execa(
+    'rollup',
+    [
+      '-c',
+      '--environment',
+      [
+        `COMMIT:${commit}`,
+        `NODE_ENV:${env}`,
+        `TARGET:${target}`,
+        formats ? `FORMATS:${formats}` : ``,
+        prodOnly ? `PROD_ONLY:true` : ``,
+        sourceMap ? `SOURCE_MAP:true` : ``
+      ]
+        .filter(Boolean)
+        .join(',')
+    ],
+    { stdio: 'inherit' }
+  )
+}
