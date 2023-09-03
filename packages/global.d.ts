21ec2bdf3dd095194a942d3c53f9712259d229cd
lmmqxyx <1368470804@qq.com>
2023-09-03 23:44:26 +0800

    optimize shared package

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -2,7 +2,7 @@
  * @Author: lmmqxyx
  * @Date: 2023-08-13 22:37:57
  * @LastEditors: lmmqxyx
- * @LastEditTime: 2023-09-03 18:21:22
+ * @LastEditTime: 2023-09-03 18:50:37
  * @FilePath: /vue3_mine/packages/global.d.ts
  * @Description:
  */
@@ -12,6 +12,8 @@
 declare var __DEV__: boolean
 declare var __TEST__: boolean
 declare var __VERSION__: string
+// compatible
+declare var __COMPAT__: boolean
 
 // used in vue/src/dev.ts
 declare var __BROWSER__: boolean
