a7509ea1dd815c4d6028d0f9788d57285658425b
lmmqxyx <1368470804@qq.com>
2023-10-16 23:52:53 +0800

    optimize the h render function

--- packages/runtime-core/src/h.ts
+++ packages/runtime-core/src/h.ts
@@ -1,16 +1,36 @@
 import { VNodeProps } from "./vnode"
 
-/*
- * @Author: lmmqxyx
- * @Date: 2023-09-13 23:55:57
- * @LastEditors: lmmqxyx
- * @LastEditTime: 2023-09-13 23:56:13
- * @FilePath: \vue3_mine\packages\runtime-core\src\h.ts
- * @Description: 
- */
 type RawProps = VNodeProps & {
   // used to differ from a single VNode object as children
   __v_isVNode?: never
   // used to differ from Array children
   [Symbol.iterator]?: never
 } & Record<string, any>
+
+// fake constructor type returned from `defineComponent`
+interface Constructor<P = any> {
+  __isFragment?: never
+  __isTeleport?: never
+  __isSuspense?: never
+  new (...args: any[]): { $props: P }
+}
+
+type RawChildren =
+  | string
+  | number
+  | boolean
+  | VNode
+  | VNodeArrayChildren
+  | (() => any)
+
+
+// The following is a series of overloads for providing props validation of
+// manually written render functions.
+
+// element
+export function h(type: string, children?: RawChildren): VNode
+export function h(
+  type: string,
+  props?: RawProps | null,
+  children?: RawChildren | RawSlots
+): VNode
