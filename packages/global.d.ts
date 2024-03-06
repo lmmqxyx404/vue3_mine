19b7a576734f57d72e5f75866427fe5e9c3a4117
lmmqxyx <1368470804@qq.com>
2024-03-06 20:09:05 +0800

    update the package

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -12,7 +12,7 @@
 declare var __DEV__: boolean
 declare var __TEST__: boolean
 declare var __VERSION__: string
-// compatible
+// used in runtime-dom patchattr()
 declare var __COMPAT__: boolean
 
 // used in vue/src/dev.ts
--- packages/runtime-dom/src/modules/attrs.ts
+++ packages/runtime-dom/src/modules/attrs.ts
@@ -1,1 +1,19 @@
-export function patchAttr(el: Element, key: string, value: any) {}
+export const xlinkNS = 'http://www.w3.org/1999/xlink'
+
+export function patchAttr(
+  el: Element,
+  key: string,
+  value: any,
+  isSVG: boolean = false,
+  instance: null
+) {
+  if (isSVG && key.startsWith('xlink:')) {
+    if (value == null) {
+      el.removeAttributeNS(xlinkNS,,key.slice(6,key.length))
+    }else{
+        el.setAttributeNS(xlinkNS,key,value)
+    }
+  }else {
+    if(__COMPAT__)
+  }
+}
